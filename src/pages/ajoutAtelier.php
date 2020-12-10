acd5bb11e12280dc1bb8a5e2be66214d789aeb4b
etheve landry <70998913+ethland-grg@users.noreply.github.com>
2020-12-10 14:05:03 +0400

    implentation formulaire dans la page ajoutAtelier

--- src/pages/ajoutAtelier.php
+++ src/pages/ajoutAtelier.php
@@ -3,6 +3,6 @@
     require_once(dirname(__DIR__)."/includes/header.php");
 ?>
 
-    <?php require_once(__ROOT__.'/src/includes/jumbotron.php');?>  
-    <?php require_once(__ROOT__.'/src/includes/listeAteliers.php');?> 
+      
+    <?php require_once(__ROOT__.'/src/includes/formAjoutAtelier.php');?> 
     <?php require_once(__ROOT__.'/src/includes/footer.php');?>
