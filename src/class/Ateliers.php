521c06db2ebab4c387a0bf7a70dd66a8037ccd69
nmd974 <jha.payet@gmail.com>
2020-12-10 12:25:55 +0400

    ajout de l'affichage des cartes selon data

---
+++ src/class/Ateliers.php
@@ -0,0 +1,21 @@
+<?php
+
+class Ateliers {
+
+    public $id;
+    public $titre;
+    public $description;
+    public $date_debut;
+    public $duree;
+    public $date_ajout;
+    public $place_disponible;
+    public $place_max;
+    public $prix;
+    public $image;
+
+    public function __construct()
+    {
+        
+    }
+}
+?>
--- src/includes/listeAteliers.php
+++ src/includes/listeAteliers.php
@@ -1,18 +1,27 @@
+<?php 
+  require_once(__ROOT__.'/src/class/Ateliers.php');
+  require_once(__ROOT__.'/src/class/AccesBDD.php');
+?>
    <!-- début section des Ateliers-->
-    <section class="container
-    ">
-
-        <!--Exemple une card Atelier 1-->
+    <section class="container">
+      <?php $ateliers = AccesBDD::ateliersData();?>
+      <?php var_dump(__ROOT__);?>
+      <?php if($ateliers !== ""):?>
+      <?php foreach ($ateliers as $key => $atelier):?>
+        <!--Atelier-->
         <div class="card my-3" style="max-width: 1000px; margin-left: auto; margin-right: auto;">
             <div class="row no-gutters">
               <div class="col-md-4">
-                <img src="images/gateau1.jpeg" class="card-img" alt="Nos cours de cuisine">
+                <img src="<?= __ROOT__ ?>/images/<?=$atelier['image']?>" class="card-img" alt="Nos cours de cuisine">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                  <h5 class="card-title"><?= $atelier['titre']?></h5>
+                  <p class="card-text"><?= $atelier['description']?></p>
+                  <p class="card-text">Commence :<?= $atelier['date_debut']?></p>
+                  <p class="card-text">Dure : <?= $atelier['duree']?> h</p>
+                  <p class="card-text"><?= $atelier['place_disponible']?>/<?= $atelier['place_max']?> places disponibles</p>
+                  <p class="card-text"><small class="text-muted"><?= $atelier['etat_ajout']?> <?= $atelier['date_ajout']?></small></p>
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
               </div>
@@ -25,72 +34,9 @@
             <hr class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         </div>
         <!--fin séparation-->
-
-        <!--Exemple une card Atelier 2-->
-        <div class="card  my-3" style="max-width: 1000px; margin-left: auto; margin-right: auto;">
-            <div class="row no-gutters">
-              <div class="col-md-4">
-                <img src="images/gateau2.jpeg" class="card-img" alt="Nos cours de cuisine">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
-        </div>
-        <!--Fin exemple 2-->
-
-        <!--séparation-->
-        <div class="row separated">
-            <hr class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        </div>
-        <!--fin séparation-->
-
-        <!--Exemple une card Atelier 3-->
-        <div class="card my-3" style="max-width: 1000px; margin-left: auto; margin-right: auto;">
-            <div class="row no-gutters">
-              <div class="col-md-4">
-                <img src="images/gateau3.jpeg" class="card-img" alt="Nos cours de cuisine">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
-        </div>
-        <!--Fin exemple 3-->
-
-        <!--séparation-->
-        <div class="row separated">
-            <hr class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        </div>
-        <!--fin séparation-->
-
-        <!--Exemple une card Atelier 4-->
-        <div class="card  my-3" style="max-width: 1000px; margin-left: auto; margin-right: auto;">
-            <div class="row no-gutters">
-              <div class="col-md-4">
-                <img src="images/gateau4.jpeg" class="card-img" alt="Nos cours de cuisine">
-              </div>
-              <div class="col-md-8">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
-        </div>
-        <!--Fin exemple 4-->
+        <?php endforeach ?>
+        <?php endif ?>
+      
 
 
     </section>
