ca04a0f55e533cbd92e0223073575e3407ec3ef5
etheve landry <70998913+ethland-grg@users.noreply.github.com>
2020-12-10 13:39:50 +0400

    modif formulaire identifiant

--- src/pages/ajoutAtelier.php
+++ src/pages/ajoutAtelier.php
@@ -6,4 +6,3 @@
     <?php require_once(__ROOT__.'/src/includes/jumbotron.php');?>  
     <?php require_once(__ROOT__.'/src/includes/listeAteliers.php');?> 
     <?php require_once(__ROOT__.'/src/includes/footer.php');?>
-
