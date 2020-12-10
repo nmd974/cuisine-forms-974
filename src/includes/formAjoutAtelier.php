9165211d5efc24ec27f340f991f02f4d9a837d34
etheve landry <70998913+ethland-grg@users.noreply.github.com>
2020-12-10 14:18:09 +0400

    page s'identifier

--- src/includes/formAjoutAtelier.php
+++ src/includes/formAjoutAtelier.php
@@ -1,12 +1,5 @@
-<!--<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ajout Atelier</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-</head> -->
-<body>
+
+
     <div class="text-center mt-5 pt-5">
         <h2>
             AJOUTEZ VOS ATELIERS
@@ -64,5 +57,4 @@
         </form>
         
     </section>
-</body>
-</html>
+
---
+++ src/pages/loging.php
@@ -0,0 +1,8 @@
+<?php 
+    $title = "Ateliers de cuisine";
+    require_once(dirname(__DIR__)."/includes/header.php");
+?>
+
+      
+    <?php require_once(__ROOT__.'/src/includes/formIdentifiant.php');?> 
+    <?php require_once(__ROOT__.'/src/includes/footer.php');?>
