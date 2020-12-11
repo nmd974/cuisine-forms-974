f63ee30493b6afd389d43cf3e2be6aab12cdb43c
nmd974 <jha.payet@gmail.com>
2020-12-11 13:40:39 +0400

    suppression fichier image control car bug

--- src/controllers/controllerAtelier.php
+++ src/controllers/controllerAtelier.php
@@ -1,6 +1,4 @@
 
-<?php require_once(__ROOT__ . '/src/controllers/imageControl.php'); // Ici j'inclut et exécute le fichiers spécifier en argument 'imageControl.php'.
-?>
 
 <!-- Créaction d'une fonction permettant de validé l'ajout d'un atelier -->
 
--- src/includes/header.php
+++ src/includes/header.php
@@ -48,7 +48,7 @@
 
 <body id="body">
   <!-- Fixed navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="../pages/home.php">
     <img src="../../images/logo.png" width="30" height="30" alt="">
   Application</a>
