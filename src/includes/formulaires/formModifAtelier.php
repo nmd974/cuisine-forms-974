98d4232c5993c6bd7a79566167a4ce1473fe54de
Grégory ALAVIN <70998888+gregory834@users.noreply.github.com>
2020-12-11 14:51:49 +0100

    Encode les données modif, encode et push vers le fichier Json.

--- src/includes/formulaires/formModifAtelier.php
+++ src/includes/formulaires/formModifAtelier.php
@@ -5,27 +5,63 @@
 <?php include(__ROOT__ . '/src/controllers/controllerAtelier.php') // Ici on inclut la fonction qui permet de controller la sécurité du formulaire
 ?>
 
-<?php if (isset($_GET['id'])) { // Je recupère la valeur $_GET_['ID'] dans le fichier atelier.json
+<?php // Si le bouton submit est cliqué je lance la fonction.
+if (isset($_POST['modifier'])) {
+
 
     $fichierDonneesAtelier = "../../data/ateliers.json"; // Ici on déterminer le chemin fichier atelier.json où se trouve les données;
 
-    $data = json_decode(file_get_contents($fichierDonneesAtelier), true); // Je décode les données JSON dans la variable $data 
+    $data = json_decode(file_get_contents($fichierDonneesAtelier), true); // Je décode les données JSON dans la variable $data
+
+
+    // parcourir le tableau récupère les valeurs $_GET.
+    foreach ($data as $key => $atelier) {
+
+        if ($atelier['id'] == $_GET["id"]) {
+
+            $data[$key]['titre']=$_POST["titre"];
+            $data[$key]['description']=$_POST["description"];
+            $data[$key]['date_ajout']=$_POST["date_ajout"];
+            $data[$key]['place_disponible']=$_POST["place_disponible"];
+            $data[$key]['date_debut']=$_POST["date__debut"];
+            $data[$key]['date_debut']=$_POST["date__debut"];
+            $data[$key]['prix']=$_POST["prix"];
+
+            file_put_contents($fichierDonneesAtelier,json_encode($data)); // ici j'encode les données $data et je les mets dans la variable $fichierDonnéeAtelier
+     
+        }
+    }
 }
 
 
 ?>
-<?php // Passe en revue le tableau $data 
+
+
+<?php if (isset($_GET['id'])) { // Je recupère la valeur $_GET_['ID'] dans le fichier atelier.json
+
+    $fichierDonneesAtelier = "../../data/ateliers.json"; // Ici on déterminer le chemin fichier atelier.json où se trouve les données;
+
+    $data = json_decode(file_get_contents($fichierDonneesAtelier), true); // Je décode les données JSON dans la variable $data
+}
+
+
+?>
+<?php // Code à commenter !!!
 foreach ($data as $atelier) :
 ?>
 
+
+
     <?php
-    if ($atelier['id'] == $_GET["id"]) : // Si 
+    if ($atelier['id'] == $_GET["id"]) : // à commenter!!!
     ?>
+
+
         <!------------------------------------------------------------------>
 
         <div class="text-center mt-5 pt-5">
             <h2>
-                AJOUTEZ VOS ATELIERS
+                MODIFIER VOS ATELIERS
             </h2>
         </div>
         <section class="container mt-5 pt-5">
@@ -75,10 +111,12 @@ foreach ($data as $atelier) :
                     <input type="file" class="form-control" id="inputGroupFile03" aria-describedby="inputGroupFileAddon03" aria-label="Upload" <?= $atelier['image'] ?>>
                 </div>
                 <div class="text-center">
-                    <button type="submit" class="btn btn-secondary">Ajouter</button>
+                    <button type="submit" name="modifier" class="btn btn-secondary">Modifier</button>
                 </div>
             </form>
 
         </section>
-    <?php endif // Termine la fonction  ?> 
-<?php endforeach // Termine la boucle foreach?>
+    <?php endif // Termine la fonction  
+    ?>
+<?php endforeach // Termine la boucle foreach
+?>
