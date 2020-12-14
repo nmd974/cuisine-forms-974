82d469726303ce6140a96856018f717820b142ed
ethland-grg <ethlandgrg@gmail.com>
2020-12-14 21:19:08 +0400

    terminera validation

---
+++ src/controllers/doneeAtelier.php
@@ -0,0 +1,58 @@
+<?php
+  
+  function validerDonneesAtelierForm($data) {
+    $champs = [ 
+      array("key" => "titre", "libele" => "'Nom de l'atelier'"), 
+      array("key" => "prix", "libele" => "'Prix de l'atelier'"),
+      array("key" => "duree", "libele" => "'Durée'"),
+      array("key" => "description", "libele" => "'Description'"),
+      array("key" => "date_debut", "libele" => "'Date début de l'atelier'"),
+      array("key" => "nombre_places", "libele" => "'Place disponibe'"),
+      array("key" => "heure_debut", "libele" => "'Heure de début'")];
+    $validation = true;
+    
+    foreach ($champs as $champ){
+      if ($data[$champ["key"]] == "") {
+        return array("valide" => false, "message" => 'Le champ '.$champ["libele"].' est obligatoir');
+      }
+      
+      if ($champ["key"] == "description" && strlen($data[$champ["key"]]) > 250 ) {
+        return array("valide" => false, "message" => 'La description ne doit pas dépasser les 250 caractères');
+      }
+      
+      if ($champ["key"] == "description" && strlen($data[$champ["key"]]) > 25 ) {
+        return array("valide" => false, "message" => 'La description ne doit pas dépasser les 250 caractères');
+      }
+   }
+    return array("valide" => true);
+  }
+
+  function ajouterAtelier($dataPost, $files){
+    $fichierDonneesAtelier = "../../data/ateliers.json";
+    $data = json_decode(file_get_contents($fichierDonneesAtelier), true);
+
+    $dataPost["titre"] = htmlspecialchars($dataPost["titre"]);
+    $dataPost["description"] = htmlspecialchars($dataPost["description"]);
+    $dataPost["prix"] = htmlentities($dataPost["prix"]);
+    $dataPost["duree"] = htmlentities($dataPost["duree"]);
+    $dataPost["date_debut"] =  htmlspecialchars($dataPost["date_debut"]);
+    $dataPost["nombre_places"] = htmlentities($dataPost["nombre_places"]);
+    $dataPost["heure_debut"] =  htmlspecialchars($dataPost["heure_debut"]);
+    $dataPost["id"] = md5(uniqid(rand(), true));
+    $dataPost["proprietaire"] = 1;
+    $dataPost["date_ajout"] = (new Datetime())->format('d-m-Y H:i:s');//sans paramettre Datetime() retourne la date et l'heure actuelle;
+    $dataPost["etat_atelier"] ="Désactivé";
+    $dataPost["participants"] =[];
+    $dataPost["modifie"] = false;
+
+    $target_dir = "..\\..\\images\\uploadedFiles\\"; // definire le chemain du telechargemet faire attention c'est différe WIN et LINUX
+    $target_file = $target_dir . basename($files["image"]["name"]);
+
+    if (move_uploaded_file($files["image"]["tmp_name"], $target_file /*télécharger le fichier et enregistrer dans le répértoire */)) {
+        $dataPost["image"] = $target_file;
+    }
+
+    array_unshift($data, $dataPost);
+    file_put_contents($fichierDonneesAtelier, json_encode($data));
+  }
+?>
--- src/includes/formulaires/formAjoutAtelier.php
+++ src/includes/formulaires/formAjoutAtelier.php
@@ -1,3 +1,17 @@
+<?php
+    require_once(__ROOT__.'/src/controllers/doneeAtelier.php');
+  if(isset($_POST["valider"])){
+    $validation = validerDonneesAtelierForm($_POST);
+    if(!$validation["valide"]){
+        $message = '<div class="alert alert-danger">'.$validation["message"].'</div>';
+        echo $message;
+    }else{
+        ajouterAtelier($_POST, $_FILES);
+        header("Location:.\cuistoManager.php");
+    }
+
+  }
+?>
 
 
     <div class="text-center mt-5 pt-5">
@@ -6,10 +20,10 @@
         </h2>
     </div>
     <section class="container mt-5 pt-5">
-        <form action="..\controllers\doneeAtelier.php" method="post" enctype="multipart/form-data">
+        <form action="" method="post" enctype="multipart/form-data">
             <div class="mb-3">
                 <label for="titre" class="form-label">Nom de l'atelier</label>
-                <input type="text" class="form-control" id="titre" aria-describedby="#" name="titre" >
+                <input type="text" class="form-control" id="titre" aria-describedby="#" name="titre" value='<?php if(isset($_POST["titre"])){ echo htmlentities($_POST["titre"]);}?>' >
                 
             </div>
             <div class="mb-3">
@@ -58,7 +72,7 @@
                 <input type="file" for="image" class="form-control" id="image" aria-describedby="inputGroupFileAddon03" aria-label="Upload" name="image">
             </div>
             <div class="text-center">
-                <button type="submit" name="valide" class="btn btn-secondary">Ajouter</button>
+                <button type="submit" name="valider" class="btn btn-secondary">Ajouter</button>
             </div>
         </form>
        
