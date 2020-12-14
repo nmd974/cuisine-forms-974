c3ab1472b486638f3724caf9e58a5a8eb587c444
olivier lebon <olivier@lebon.dev>
2020-12-14 14:58:38 +0400

    Revert "Update authentification.php"
    This reverts commit df5028bcbd7347caf544abde51c39e1175e736fb, reversing
    changes made to 70f98bbe8871566d55c03f33b5782d7154a396fa.

--- src/controllers/authentification.php
+++ src/controllers/authentification.php
@@ -1,14 +1,9 @@
 <?php
 
 
-        $verificationStatus = false;
-        $role = null;
-        $message = '<div class="alert alert-success"> Connexion réussie</div>';
-        $email = htmlentities($_POST['email'], ENT_QUOTES);  //début hash password
-        $password = htmlentities($_POST['password'], ENT_QUOTES);
-        //sécurisation password par hash
-        $passHash = password_hash($password, PASSWORD_DEFAULT);
-        $passwordValid = password_verify($_POST['password'], $passHash); // On crypte le mot
+//Ici on contrôle le mot de passe et email de l'utilisateur
+function validationConnexion()
+{
 
     $verificationStatus = false;
     $role = null;
@@ -19,38 +14,29 @@
     $passwordValid = password_verify($_POST['password'], $passHash); // On crypte le mot
     echo var_dump($passwordValid);
 
-        $data = json_decode($datajson, true);
-        
-        foreach ($data as $value) {
-            //recherche correspondance par mail
-            if($value['emailUser'] == $email){
-                //recherche correspondance par password
-                if($value['passwordUser'] == $passwordValid){
-                    //on vérifie si role cuisinier
-                    if($value['role'] == "cuisinier"){
-                        $verificationStatus = true;
-                        $_SESSION['cuisinierLoggedIn'] = true;
-                        $_SESSION['id'] = $value['id'];
-                        $_SESSION['ateliers'] = $value['ateliers'];
-                        header('Location: ./cuistoManager.php');  
-                    //sinon c'est un user    
-                    }else{
-                        $role = 'user';
-                        $verificationStatus = true;
-                        $_SESSION['particulierLoggedIn'] = true;
-                        $_SESSION['id'] = $value['id'];
-                        $_SESSION['ateliers'] = $value['ateliers'];
-                        header('Location: ./home.php');      
-                    }
+    $datajson = file_get_contents("../../data/users.json");
+
+    $data = json_decode($datajson, true);
+
+    foreach ($data as $value) {
+        if ($value['emailUser'] == $email) {
+            if ($value['passwordUser'] == $passwordValid) {
+                if ($value['role'] == "cuisinier") {
+                    $verificationStatus = true;
+                    $_SESSION['cuisinierLoggedIn'] = true;
+                    $_SESSION['id'] = $value['id'];
+                    $_SESSION['ateliers'] = $value['ateliers'];
+                    header('Location: ./cuistoManager.php');
+                } else {
+                    $role = 'user';
+                    $verificationStatus = true;
+                    $_SESSION['particulierLoggedIn'] = true;
+                    $_SESSION['id'] = $value['id'];
+                    $_SESSION['ateliers'] = $value['ateliers'];
+                    header('Location: ./home.php');
                 }
             }
         }
-        //alerte condition incorret !
-        if(!$verificationStatus){
-            $message = '<div class="alert alert-danger">Compte ou mot de passe incorrects !</div>';
-        }
-        return array($verificationStatus, $role, $message);
-
     }
     if (!$verificationStatus) {
         $message = '<div class="alert alert-danger">Compte ou mot de passe incorrects !</div>';
