64215a9de82f39c5cd0adf7505cbdd42b5d6b916
olivier lebon <olivier@lebon.dev>
2020-12-16 11:11:13 +0400

    Update authentification.php

--- src/controllers/authentification.php
+++ src/controllers/authentification.php
@@ -11,6 +11,7 @@ function validationConnexion(){
     $message = '<div class="alert alert-success"> Connexion réussie</div>';
     //attribution input dans une variable
     $email = htmlentities($_POST['email'], ENT_QUOTES);
+    $email = strtolower($_POST['email']);
     //attribution input dans une variable
     $password = htmlentities($_POST['password'], ENT_QUOTES);
     //verification cryptage du mot passe
